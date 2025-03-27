350cb9dd3555ca9920ddb4ecd7e476426588298e
chenjun <chenjun@infrawaves.com>
2025-03-27 14:46:08 +0800

    chen

---
+++ test/script/auto_script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+TEST_DIR="/workspace/FlagCX"
+BUILD_DIR="$TEST_DIR/build"
+
+mkdir -p $BUILD_DIR
+
+cd $TEST_DIR
+
+make USE_NVIDIA=1
+
+if [ $? -ne 0 ]; then
+    echo "Compilation failed!"
+    exit 1
+fi
+
+cd $TEST_DIR/test/perf
+
+make USE_NVIDIA=1
+
+if [ $? -ne 0 ]; then
+    echo "Test compilation failed!"
+    exit 1
+fi
+
+
+mpirun -np 8 ./test_allreduce -b 128M -e 8G -f 2
+
+if [ $? -ne 0 ]; then
+    echo "Test execution failed!"
+    exit 1
+fi
+
+
