ea772d2f07991132bbb01fb8d1b31ea987b5b3a9
chenjun <chenjun@infrawaves.com>
2025-03-28 15:33:53 +0800

    update workflow

--- test/script/auto_script.sh
+++ test/script/auto_script.sh
@@ -3,7 +3,6 @@ BUILD_DIR="build"
 
 mkdir -p $BUILD_DIR
 
-
 make USE_NVIDIA=1
 
 if [ $? -ne 0 ]; then
@@ -12,7 +11,6 @@ if [ $? -ne 0 ]; then
 fi
 
 cd test/perf
-
 make USE_NVIDIA=1
 
 if [ $? -ne 0 ]; then
@@ -20,10 +18,66 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-mpirun -np 8 ./test_allreduce -b 128M -e 8G -f 2
 
+mpirun -np 8 ./test_sendrecv -b 128M -e 2G -f 2 -p 1
 if [ $? -ne 0 ]; then
-    echo "Test execution failed!"
+    echo "test_sendrecv execution failed!"
     exit 1
 fi
+
+mpirun -np 8 ./test_allreduce -b 128M -e 2G -f 2 -p 1
+if [ $? -ne 0 ]; then
+    echo "test_allreduce execution failed!"
+    exit 1
+fi
+
+mpirun -np 8 ./test_allgather -b 128M -e 2G -f 2 -p 1
+if [ $? -ne 0 ]; then
+    echo "test_allgather execution failed!"
+    exit 1
+fi
+
+mpirun -np 8 ./test_reducescatter -b 128M -e 2G -f 2 -p 1
+if [ $? -ne 0 ]; then
+    echo "test_reducescatter execution failed!"
+    exit 1
+fi
+
+mpirun -np 8 ./test_alltoall -b 128M -e 2G -f 2 -p 1
+if [ $? -ne 0 ]; then
+    echo "test_alltoall execution failed!"
+    exit 1
+fi
+
+mpirun -np 8 ./test_alltoallv -b 128M -e 2G -f 2 -p 1
+if [ $? -ne 0 ]; then
+    echo "test_alltoallv execution failed!"
+    exit 1
+fi
+
+mpirun -np 8 ./test_broadcast -b 128M -e 2G -f 2 -p 1
+if [ $? -ne 0 ]; then
+    echo "test_broadcast execution failed!"
+    exit 1
+fi
+
+mpirun -np 8 ./test_gather -b 128M -e 2G -f 2 -p 1
+if [ $? -ne 0 ]; then
+    echo "test_gather execution failed!"
+    exit 1
+fi
+
+mpirun -np 8 ./test_scatter -b 128M -e 2G -f 2 -p 1
+if [ $? -ne 0 ]; then
+    echo "test_scatter execution failed!"
+    exit 1
+fi
+
+mpirun -np 8 ./test_reduce -b 128M -e 8G -f 2 -p 1
+if [ $? -ne 0 ]; then
+    echo "test_reduce execution failed!"
+    exit 1
+fi
+
+echo "All tests completed successfully!"
 
