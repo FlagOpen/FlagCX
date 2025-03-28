56550ceb9eb66503caa013b1aa3526e5dd81140a
chenjun <chenjun@infrawaves.com>
2025-03-28 16:14:54 +0800

    update workflow

--- test/script/auto_script.sh
+++ test/script/auto_script.sh
@@ -3,6 +3,7 @@ BUILD_DIR="build"
 
 mkdir -p $BUILD_DIR
 
+MPI_HOME=/usr/local/mpi 
 make USE_NVIDIA=1
 
 if [ $? -ne 0 ]; then
@@ -55,25 +56,25 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-mpirun -np 8 ./test_broadcast -b 128M -e 2G -f 2 -p 1
+mpirun -np 8 ./test_broadcast -b 128M -e 2G -f 2 -r 0 -p 1
 if [ $? -ne 0 ]; then
     echo "test_broadcast execution failed!"
     exit 1
 fi
 
-mpirun -np 8 ./test_gather -b 128M -e 2G -f 2 -p 1
+mpirun -np 8 ./test_gather -b 128M -e 2G -f 2 -r 0 -p 1
 if [ $? -ne 0 ]; then
     echo "test_gather execution failed!"
     exit 1
 fi
 
-mpirun -np 8 ./test_scatter -b 128M -e 2G -f 2 -p 1
+mpirun -np 8 ./test_scatter -b 128M -e 2G -f 2 -r 0 -p 1
 if [ $? -ne 0 ]; then
     echo "test_scatter execution failed!"
     exit 1
 fi
 
-mpirun -np 8 ./test_reduce -b 128M -e 8G -f 2 -p 1
+mpirun -np 8 ./test_reduce -b 128M -e 8G -f 2 -r 0 -p 1
 if [ $? -ne 0 ]; then
     echo "test_reduce execution failed!"
     exit 1
