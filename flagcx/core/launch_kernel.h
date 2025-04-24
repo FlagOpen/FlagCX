55703570cd3c077dd8166a940393a396e7e41c96
ceci3 <592712189@qq.com>
2025-04-24 19:51:59 +0800

    update

--- flagcx/core/launch_kernel.h
+++ flagcx/core/launch_kernel.h
@@ -1,24 +1,24 @@
 #ifndef FLAGCX_LAUNCH_KERNEL_H_
 #define FLAGCX_LAUNCH_KERNEL_H_
 
-#include "adaptor.h"
-#include "debug.h"
-#include "param.h"
 #include "topo.h"
-#include "utils.h"
-#include <dlfcn.h>
+#include "debug.h"
+#include <stdlib.h>
 #include <getopt.h>
-#include <iomanip>
-#include <iostream>
-#include <math.h>
 #include <memory.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+#include <iomanip>
 #include <unistd.h>
+#include <dlfcn.h>
+#include "adaptor.h"
+#include "utils.h"
+#include "param.h"
 
-struct hostLaunchArgs {
-  volatile bool stopLaunch;
-  volatile bool retLaunch;
+struct hostLaunchArgs{
+    volatile bool stopLaunch;
+    volatile bool retLaunch;
 };
 
 void cpuAsyncLaunch(void *_args);
