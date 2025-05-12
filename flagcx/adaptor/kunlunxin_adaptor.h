449e570501a63a8baf8d6dd0ffb73ed30edb37a1
liuyuxuan15 <liuyuxuan15@example.com>
2025-05-12 19:35:39 +0800

    先保存一下对于runtime的适配修改，尚未尝试编译，头文件中通信库的实例也尚未修改

---
+++ flagcx/adaptor/kunlunxin_adaptor.h
@@ -0,0 +1,37 @@
+/*************************************************************************
+ * Copyright (c) 2025 by MetaX Integrated Circuits (Shanghai) Co., Ltd. All Rights Reserved.
+ ************************************************************************/
+
+#ifdef USE_METAX_ADAPTOR
+
+#include <map>
+
+#include <bkcl.h>
+#include "cuda.h"
+#include <cuda_runtime.h>
+
+#include "adaptor.h"
+#include "alloc.h"
+#include "comm.h"
+#include "flagcx.h"
+
+struct flagcxInnerComm {
+    // xcclComm_t base; // todo:这个看着是我们的BKCLContext？
+};
+
+struct flagcxStream {
+    cudaStream_t base;
+};
+
+struct flagcxEvent {
+    cudaEvent_t base;  // todo： 这个对应xpu是啥 init.h 文件里只搜索到了rdmaenvent的变量，我感觉明显不对。
+};
+
+#define DEVCHECK(func)                                                         \
+  {                                                                            \
+    int ret = func;                                                            \
+    if (ret != cudaSuccess)                                                      \
+      return flagcxUnhandledDeviceError;                                       \
+  }
+
+#endif // USE_METAX_ADAPTOR
