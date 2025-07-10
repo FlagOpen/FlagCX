a9bba749773297264f06c5953fc98acd697a1134
titikim <1595992831@qq.com>
2025-07-10 20:39:10 +0800

    m

--- plugin/torch/flagcx/include/stream_guard_flagcx.hpp
+++ plugin/torch/flagcx/include/stream_guard_flagcx.hpp
@@ -12,6 +12,7 @@
 #include <c10/cuda/impl/CUDAGuardImpl.h>
 #include <cuda_runtime.h>
 #elif USE_ASCEND_ADAPTOR
+#include "torch_npu/csrc/core/npu/NPUStream.h"
 #include "torch_npu/csrc/core/npu/NPUGuard.h"
 #include "torch_npu/csrc/core/npu/impl/NPUGuardImpl.h"
 #include "torch_npu/csrc/core/npu/NPUEvent.h"
@@ -72,7 +73,6 @@ public:
 	guard_(c10_npu::getStreamFromExternal(*(aclrtStream *)stream, deviceId_))
 #endif
   {
-    std::cout<<"/**********************  enter guard_(c10_npu::getCurrentNPUStream(deviceId)) ********************/"<<std::endl;
   }
   ~flagcxStreamGuard() = default;
 
@@ -104,11 +104,9 @@ public:
     guard_.reset_stream(
         at::cuda::getStreamFromExternal(*(cudaStream_t *)stream, deviceId_));
 #elif USE_ASCEND_ADAPTOR
-        // guard_ = c10_npu::getNPUStreamFromPool(deviceId_);
-	std::cout<<"/**********************  enter guard_(c10_npu::getCurrentNPUStream(deviceId)) ********************/"<<std::endl;
 	guard_.reset_stream(c10_npu::getStreamFromExternal(*(aclrtStream *)stream, deviceId_));
-//	guard_ = c10_npu::getStreamFromExternal(*(aclrtStream *)stream, deviceId_);
-//	guard_ = c10_npu::getCurrentNPUStream(deviceId_);
+	//guard_.reset_stream(c10_npu::getNPUStreamFromPool(deviceId_));
+        //guard_.reset_stream(c10_npu::getCurrentNPUStream(deviceId_));
 #endif
     currentStream_ = stream;
   }
