76adaee60cda269a0f11b720d06445038c5dcc62
chencjcj <chencjcj>
2025-08-18 03:19:41 +0000

    Add net_socket for netplugin

--- flagcx/core/net.cc
+++ flagcx/core/net.cc
@@ -646,76 +646,6 @@ flagcxResult_t flagcxNetInit(struct flagcxHeteroComm* comm) {
   return flagcxSuccess;
 }
 
-
-// flagcxResult_t flagcxProxySend(sendNetResources *resources, void *data,
-//                                size_t size, flagcxProxyArgs *args) {
-//   if (deviceKernel) {
-//     deviceAdaptor->deviceMemcpy(args->hEventReady, args->dEventReady, 1,
-//                                 flagcxMemcpyDeviceToHost, resources->cpStream,
-//                                 NULL);
-//   }
-//   if (!__atomic_load_n(&args->hEventReady, __ATOMIC_RELAXED))
-//     return flagcxSuccess;
-//   if (args->transmitted < args->chunkSteps) {
-//     int stepMask = args->sendStepMask;
-
-//     if (args->waitCopy < args->chunkSteps &&
-//         args->waitCopy - args->transmitted < MAXSTEPS) {
-//       int step = args->waitCopy & stepMask;
-//       args->subs[step].stepSize =
-//           std::min(args->chunkSize, size - args->totalCopySize);
-//       args->subs[step].stepBuff = resources->buffers[0] + (CHUNKSIZE * step);
-//       deviceAdaptor->deviceMemcpy(
-//           args->subs[step].stepBuff, (char *)data + args->totalCopySize,
-//           args->subs[step].stepSize, flagcxMemcpyDeviceToDevice,
-//           resources->cpStream, args->subs[step].copyArgs);
-//       deviceAdaptor->eventRecord(resources->cpEvents[step],
-//                                  resources->cpStream);
-//       args->totalCopySize += args->subs[step].stepSize;
-//       args->waitCopy++;
-//     }
-
-//     if (args->copied < args->waitCopy) {
-//       int step = args->copied & stepMask;
-//       if (deviceAdaptor->eventQuery(resources->cpEvents[step]) ==
-//           flagcxSuccess) {
-//         args->copied++;
-//       }
-//     }
-//     if (args->posted < args->copied) {
-//       void *req = NULL;
-//       // printf("isend start");
-//       resources->flagcxNet->isend(resources->netSendComm,
-//                         args->subs[args->posted & stepMask].stepBuff,
-//                         args->subs[args->posted & stepMask].stepSize, 0,
-//                         resources->mhandles[0], &req);
-//       // printf("isend successfully");
-//       if (req) {
-//         args->subs[args->posted++ & stepMask].requests[0] = req;
-//       }
-//     }
-
-//     if (args->transmitted < args->posted) {
-//       void *req = args->subs[args->transmitted & stepMask].requests[0];
-//       int done = 0, sizes;
-//       resources->flagcxNet->test(req, &done, &sizes);
-//       if (done) {
-//         args->transmitted++;
-//       }
-//     }
-//   } else {
-//     __atomic_store_n(args->hlArgs, 1, __ATOMIC_RELAXED);
-//     args->done = true;
-//     if (deviceKernel) {
-//       deviceAdaptor->deviceMemcpy(args->dlArgs, args->hlArgs, 1,
-//                                   flagcxMemcpyHostToDevice, resources->cpStream,
-//                                   NULL);
-//     }
-//   }
-
-//   return flagcxSuccess;
-// }
-
 flagcxResult_t flagcxProxySend(sendNetResources *resources, void *data,
                                size_t size, flagcxProxyArgs *args) {
   if (deviceKernel) {
@@ -848,8 +778,6 @@ flagcxResult_t flagcxProxyRecv(recvNetResources *resources, void *data,
     if (args->flushed < args->postFlush) {
       void *req = args->subs[args->flushed & stepMask].requests[0];
       int done = 0, sizes;
-      // resources->flagcxNet->test(req, &done, &sizes);
-            // Check if this is a Socket network dummy request
       if (resources->flagcxNet == &flagcxNetSocket && req == (void*)0x1) {
         done = 1; 
         sizes = 0;
@@ -899,100 +827,6 @@ flagcxResult_t flagcxProxyRecv(recvNetResources *resources, void *data,
   }
   return flagcxSuccess;
 }
-
-
-
-// flagcxResult_t flagcxProxyRecv(recvNetResources *resources, void *data,
-//                                size_t size, flagcxProxyArgs *args) {
-//   if (deviceKernel) {
-//     deviceAdaptor->deviceMemcpy(args->hEventReady, args->dEventReady, 1,
-//                                 flagcxMemcpyDeviceToHost, resources->cpStream,
-//                                 NULL);
-//   }
-//   if (!__atomic_load_n(&args->hEventReady, __ATOMIC_RELAXED))
-//     return flagcxSuccess;
-//   if (args->copied < args->chunkSteps) {
-//     int stepMask = args->sendStepMask;
-//     if (args->posted < args->chunkSteps &&
-//         args->posted - args->copied < MAXSTEPS) {
-//       int tags[8] = {0};
-//       void *req = NULL;
-//       args->subs[args->posted & stepMask].stepSize =
-//           std::min(args->chunkSize, size - args->totalPostSize);
-//       args->subs[args->posted & stepMask].stepBuff =
-//           resources->buffers[0] + CHUNKSIZE * (args->posted & stepMask);
-//       resources->flagcxNet->irecv(resources->netRecvComm, 1,
-//                         &args->subs[args->posted & stepMask].stepBuff,
-//                         (int *)&args->subs[args->posted & stepMask].stepSize,
-//                         tags, resources->mhandles, &req);
-//       if (req) {
-//         args->subs[args->posted & stepMask].requests[0] = req;
-//         args->totalPostSize += args->subs[args->posted++ & stepMask].stepSize;
-//       }
-//     }
-
-//     if (args->transmitted < args->posted) {
-//       void *req = args->subs[args->transmitted & stepMask].requests[0];
-//       int done = 0, sizes;
-//       resources->flagcxNet->test(req, &done, &sizes);
-//       if (done) {
-//         args->transmitted++;
-//       }
-//     }
-
-//     if (args->postFlush < args->transmitted) {
-//       void *req = NULL;
-//       void *allData[] = {args->subs[args->postFlush & stepMask].stepBuff};
-//       resources->flagcxNet->iflush(resources->netRecvComm, 1, allData,
-//                          &args->subs[args->postFlush & stepMask].stepSize,
-//                          resources->mhandles, &req);
-//       if (req) {
-//         args->subs[args->postFlush++ & stepMask].requests[0] = req;
-//       }
-//     }
-
-//     if (args->flushed < args->postFlush) {
-//       void *req = args->subs[args->flushed & stepMask].requests[0];
-//       int done = 0, sizes;
-//       resources->flagcxNet->test(req, &done, &sizes);
-//       if (done) {
-//         args->flushed++;
-//       }
-//     }
-
-//     if (args->waitCopy < args->flushed) {
-//       int step = args->waitCopy & stepMask;
-//       deviceAdaptor->deviceMemcpy(
-//           (char *)data + args->totalCopySize, args->subs[step].stepBuff,
-//           args->subs[step].stepSize, flagcxMemcpyDeviceToDevice,
-//           resources->cpStream, args->subs[step].copyArgs);
-//       deviceAdaptor->eventRecord(resources->cpEvents[step],
-//                                  resources->cpStream);
-//       args->totalCopySize += args->subs[step].stepSize;
-//       args->waitCopy++;
-//     }
-
-//     if (args->copied < args->waitCopy) {
-//       int step = args->copied & stepMask;
-//       if (deviceAdaptor->eventQuery(resources->cpEvents[step]) ==
-//           flagcxSuccess) {
-//         args->copied++;
-//       }
-//     }
-
-//   } else {
-//     __atomic_store_n(args->hlArgs, 1, __ATOMIC_RELAXED);
-//     args->done = true;
-//     if (deviceKernel) {
-//       deviceAdaptor->deviceMemcpy(args->dlArgs, args->hlArgs, 1,
-//                                   flagcxMemcpyHostToDevice, resources->cpStream,
-//                                   NULL);
-//     }
-//   }
-
-//   return flagcxSuccess;
-// }
-
 
 flagcxResult_t flagcxSendProxyFree(sendNetResources *resources) {
   resources->flagcxNet->deregMr(resources->netSendComm, resources->mhandles[0]);
