7d77aa436d06786f9e329b8a374dfaeff1d43548
chenqing <chenqing@infrawaves.com>
2025-10-21 13:00:24 +0000

    Add the feature of automatically generating candidates.

--- flagcx/adaptor/tuner/tuner_util.h
+++ flagcx/adaptor/tuner/tuner_util.h
@@ -3,8 +3,56 @@
 
 #include "tuner.h" // struct flagcxEnvConfig
 #include <vector>
+#include <string>
 
 // This is a demonstration function that provide a way to load all config list for a specific GPU.
-flagcxResult_t loadConfigList(std::vector<struct flagcxEnvConfig> &cfgList);
+
+struct EnvVar {
+    std::string name;
+    std::vector<std::string> choices;
+    std::string defaultValue;
+    EnvVar(std::string n="") : name(std::move(n)) {}
+    EnvVar(std::string n, std::vector<std::string> c, std::string d = "")
+      : name(std::move(n)), choices(std::move(c)), defaultValue(std::move(d)) {}
+};
+
+flagcxResult_t generateCandidate(std::vector<struct flagcxEnvConfig> &cfgList);
+std::vector<struct flagcxEnvConfig> generateCandidate(std::vector<EnvVar> vars);
+static void safeStrCopy(char *dst, size_t dstSize, const std::string &src);
+
+#ifdef USE_NVIDIA_ADAPTOR
+
+static EnvVar algo(
+    "NCCL_ALGO",
+    {"ring", "tree"},
+    "ring"
+);
+
+static EnvVar proto(
+    "NCCL_PROTO",
+    {"LL", "LL128", "Simple"},
+    "Simple"
+);
+
+static EnvVar thread(
+    "NCCL_NTHREADS",
+    {"128", "256"},
+    "256"
+);
+
+static EnvVar minChannel(
+    "NCCL_MIN_NCHANNELS",
+    {"16", "32"},
+    "16"
+);
+
+static EnvVar chunkSize(
+    "NCCL_P2P_NET_CHUNKSIZE",
+    {"1024", "2048"},
+    "1024"
+);
+static std::vector<EnvVar> vars = {algo, proto, thread, minChannel, chunkSize};
+
+#endif
 
 #endif // end include guard
